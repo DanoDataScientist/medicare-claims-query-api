a51172ed7c9b2e7fdf63250c1d8bb94079e0eeb9
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-05 18:55:57 -0800

    Create empty load_data.py - Will contain code to load the CMS data into a local or remote   Postgres DB.

---
+++ db/load_data.py
@@ -0,0 +1,6 @@
+"""Load the CMS 2008-2010 Medicare Beneficiary Summary tables into Postgres.
+
+See https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable
+-Public-Use-Files/SynPUFs/DE_Syn_PUF.html
+for more info on the data.
+"""
