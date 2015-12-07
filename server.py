3a086fdeb34b1e9cc0258e4cba4ffa26977a4ce0
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-07 01:59:01 -0800

    Remove all error logging on production to try to get DB to connect

--- server.py
+++ server.py
@@ -20,23 +20,19 @@ TABLE_NAME = dbconfig.db_tablename
 locale.setlocale(locale.LC_ALL, '')  # For formatting numbers with commas
 
 
-# Setup logging to Gunicorn
-@app.before_first_request
-def setup_logging():
-    if not app.debug:
-        app.logger.addHandler(logging.StreamHandler())
-        app.logger.setLevel(logging.DEBUG)
-
-
 @app.route('/')
 def hello_world():
     num_rows = 0  # Default value
-    con, cur = cursor_connect(db_dsn)
-    sql = "SELECT COUNT(*) FROM {0}".format(TABLE_NAME)
-    cur.execute(sql, TABLE_NAME)
-    result = cur.fetchone()
-    num_rows = int(result[0])
-    return "Hello World! I can access {0:,d} rows of data!".format(num_rows)
+    try:
+        con, cur = cursor_connect(db_dsn)
+        sql = "SELECT COUNT(*) FROM {0}".format(TABLE_NAME)
+        cur.execute(sql, TABLE_NAME)
+        result = cur.fetchone()
+        num_rows = int(result[0])
+    except (psycopg2.Error, ValueError):
+        num_rows = 0
+    finally:
+        return "Hello World! I can access {0:,d} rows of data!".format(num_rows)
 
 
 if __name__ == '__main__':
@@ -46,8 +42,6 @@ if __name__ == '__main__':
         db_dsn = "host={0} dbname={1} user={2} password={3}".format(
             dbconfig.rds_dbhost, dbconfig.rds_dbname, dbconfig.rds_dbuser,
             dbconfig.rds_dbpass)
-        # Propagate exceptions to Gunicorn log: /var/log/gunicorn/error.log
-        app.config['PROPAGATE_EXCEPTIONS'] = True
         app.run()
     else:
         # Running dev server...
