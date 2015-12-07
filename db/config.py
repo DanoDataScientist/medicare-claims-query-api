e5308e6052847a5460b9104dc2135fb8cb8e64be
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-07 01:24:51 -0800

    Fix even dumber mistake - using Vagrant credentials for AWS DB in app

--- db/config.py
+++ db/config.py
@@ -9,7 +9,7 @@ rds_dbpass = rds_password.rds_pass  # Set this in db/rds_password.py
 # These settings do not need to be changed
 vagrant_dbhost = "localhost"
 vagrant_dbname = "beneficiary_data"  # Keep lowercase
-vagrant_dbuser = "nikhil"
+vagrant_dbuser = "vagrant"
 vagrant_dbpass = None
 
 # Global table name to use on RDS and Vagrant
