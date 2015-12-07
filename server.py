0a05ea6e070e2ad0afedf64374bbf93ae2c715a8
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-06 23:18:32 -0800

    Update app to use core utilities and DB files to connect to DB - Outputs number of rows in Vagrant DB on '/' when running   dev sever.

--- server.py
+++ server.py
@@ -1,18 +1,44 @@
-from flask import Flask
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
+
+import psycopg2
+from flask import Flask
+
+from core.utilities import cursor_connect
+from db import config as dbconfig
 
 app = Flask(__name__)
 
+TABLE_NAME = dbconfig.db_tablename
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! How are you? I bet you are doing well!'
+    num_rows = '<error>'  # Default value
+    try:
+        con, cur = cursor_connect(db_dsn)
+        sql = "SELECT COUNT(*) FROM {0}".format(TABLE_NAME)
+        cur.execute(sql, TABLE_NAME)
+        result = cur.fetchone()
+        num_rows = result[0]
+    except (Exception, psycopg2.Error):
+        pass
+    finally:
+        return "Hello World! I can access {0:,} rows of data!".format(num_rows)
 
 
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.realpath(__file__))
     if os.path.isfile(os.path.join(current_dir, 'PRODUCTION')):
+        # Production environment
+
         app.run()
     else:
         # Running dev server...
+        db_dsn = "host={0} dbname={1} user={2}".format(dbconfig.vagrant_dbhost,
+                                                       dbconfig.vagrant_dbname,
+                                                       dbconfig.vagrant_dbuser)
         app.run(host='0.0.0.0', debug=True)
